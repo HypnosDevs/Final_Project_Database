57a1e31aff0fc3b546e4a8a0ae7513c02d00d6b5
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-04-28 14:00:30 +0700

    add product schema

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,38 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 const env = process.env;
 
+const Product = require('./Models/Product.js')
+
+app.use(express.json());
+
+
+app.get('/Product/getProduct', async (req, res) => {
+    try {
+        const data = await Product.find()
+        res.send(data);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({ message: err.message });
+    }
+})
+
+app.post('/Product/addProduct', async (req, res) => {
+    try {
+        const newProd = new Product(req.body);
+        await newProd.save();
+        res.send(req.body)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({ message: err.message });
+    }
+})
+
 app.get('/', (req, res) => {
     res.send("Hello world")
 })
+
+
 
 const database = () => {
     try {
