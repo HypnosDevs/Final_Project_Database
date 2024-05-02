113f169a7af16bb613e3a8793837fd8c4ebdff69
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-05-02 16:51:23 +0700

    update routes orderitems

--- Backend/Routes/orderItem.js
+++ Backend/Routes/orderItem.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getAllOrderItem, getOrderItem, addOrderItem, deleteOrderItem } = require('../Controllers/orderItem');
+const { getAllOrderItem, getOrderItem, addOrderItem, deleteOrderItem, getProductFromOrderItem } = require('../Controllers/orderItem');
 const router = express.Router()
 
 router.get('/getAllsOrderItem', getAllOrderItem);
 
 router.get('/getOrderItem/:order_id/:product_id', getOrderItem)
+
+router.get('/getProductFromOrderItem/:order_id/:product_id', getProductFromOrderItem)
+
 
 router.post('/addOrderItem/:order_id/:product_id', addOrderItem)
 
