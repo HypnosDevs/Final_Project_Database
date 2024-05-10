d56c355a7e09f1bf5ab3468c1c70d391dda7b7a6
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-05-10 21:31:26 +0700

    fixed cart.js shoppingItem delete shoppingCart

--- Backend/Routes/shoppingCart.js
+++ Backend/Routes/shoppingCart.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const { getAllShoppingcart, getAllShoppingcartFromuser, getActiveShoppingcartFromuser, getShoppingCart, addShoppingCart, deleteShoppingCart } = require('../Controllers/shoppingCart');
-const router = express.Router()
+// const express = require('express');
+// const { getAllShoppingcart, getAllShoppingcartFromuser, getActiveShoppingcartFromuser, getShoppingCart, addShoppingCart, deleteShoppingCart } = require('../Controllers/shoppingCart');
+// const router = express.Router()
 
-router.get('/getAllShoppingcart', getAllShoppingcart);
+// router.get('/getAllShoppingcart', getAllShoppingcart);
 
-router.get('/getAllShoppingcartFromuser/:user_id', getAllShoppingcartFromuser);
+// router.get('/getAllShoppingcartFromuser/:user_id', getAllShoppingcartFromuser);
 
-router.get('/getActiveShoppingcartFromuser/:user_id', getActiveShoppingcartFromuser);
+// router.get('/getActiveShoppingcartFromuser/:user_id', getActiveShoppingcartFromuser);
 
-router.get('/getShoppingCart/:shoppingcart_id', getShoppingCart)
+// router.get('/getShoppingCart/:shoppingcart_id', getShoppingCart)
 
-router.post('/addShoppingCart/:user_id', addShoppingCart)
+// router.post('/addShoppingCart/:user_id', addShoppingCart)
 
-router.delete('/deleteShoppingCart/:shoppingcart_id', deleteShoppingCart)
+// router.delete('/deleteShoppingCart/:shoppingcart_id', deleteShoppingCart)
 
-module.exports = router
+// module.exports = router
