c6b9c4e6c11d7750f2c979cd37960934c7aa0d6d
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-05-25 14:08:27 +0700

    schema username,password

--- Backend/db/Schema.sql
+++ Backend/db/Schema.sql
@@ -4,6 +4,8 @@ use test_nodejs;
 
 CREATE TABLE IF NOT EXISTS user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
     firstname VARCHAR(100) NOT NULL,
     lastname VARCHAR(100) NOT NULL,
     gender VARCHAR(10),
